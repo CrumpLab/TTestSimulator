ca97a38fbc0fc2787dfca3255e3fbb85839cd5ef
Matt Crump <crumpmj@gmail.com>
2015-03-06 19:38:50 -0500

    Create server.r

---
+++ server.r
@@ -0,0 +1,19 @@
+shinyServer(function(input, output) {
+  output$plot1 <- renderPlot({
+    pSave<-c()
+    n1=as.numeric(input$n1)
+    m1=as.numeric(input$m1)
+    s1=as.numeric(input$s1)
+    n2=as.numeric(input$n2)
+    m2=as.numeric(input$m2)
+    s2=as.numeric(input$s2)
+    for(i in 1:input$Runs){
+      x1<-rnorm(n1,m1,s1)
+      x2<-rnorm(n2,m2,s2)
+      pSave<-c(pSave,t.test(x1,x2,var.equal=TRUE)$p.value)
+    }
+    hist(pSave,breaks=seq(0,1,.05))
+    
+    #hist(rnorm(input$text))
+  })
+})
