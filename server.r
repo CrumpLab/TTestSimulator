252f31f205b9e6d15e1848d98992bbcb4eeb0279
Matt Crump <crumpmj@gmail.com>
2015-03-07 08:42:28 -0500

    Update server.r

--- server.r
+++ server.r
@@ -7,13 +7,14 @@ shinyServer(function(input, output) {
     n2=as.numeric(input$n2)
     m2=as.numeric(input$m2)
     s2=as.numeric(input$s2)
-    for(i in 1:input$Runs){
+    for(i in 1:as.numeric(input$Runs)){
       x1<-rnorm(n1,m1,s1)
       x2<-rnorm(n2,m2,s2)
       pSave<-c(pSave,t.test(x1,x2,var.equal=TRUE)$p.value)
     }
     hist(pSave,breaks=seq(0,1,.05))
-    
-    #hist(rnorm(input$text))
+    Nsig<-length(pSave[pSave<.05])/as.numeric(input$Runs)
+    output$text1 <-renderText(Nsig)
   })
+  
 })
